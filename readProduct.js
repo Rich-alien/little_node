404aa00e75c91fe2a3d736a639a98de9944e7e13
rich-alien <Richalien@yandex.ru>
2020-11-29 00:43:17 +0400

    удалены комментария и не использующиеся модули

--- readProduct.js
+++ readProduct.js
@@ -1,13 +1,7 @@
-const fs = require('fs'),
-    path = require('path'),
-    product = require('./state/data')
-// let json;
-// // let json = '[{"title":"title1","description":"description1"},{"title":"title2","description":"description2"}]' тут будет наши данные
-// console.table(JSON.parse(json));
-// чтение данных
+const product = require('./state/data')
 function readFile(){
     console.log("--> Чтение товара")
-    console.table(product.product);
+    console.table(product);
 }
 module.exports={
     readFile,
