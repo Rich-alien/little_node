35e2550c054e714a235f6cf50bbb1358a93ce415
rich-alien <Richalien@yandex.ru>
2020-12-03 19:20:13 +0400

    изменен подход

--- deleteProduct.js
+++ deleteProduct.js
@@ -1,24 +1,18 @@
 const get = require("./getInput");
-const product = require('./state/data');
-const fs = require("fs");
-const path = require("path");
 const readline = require("readline");
+const {setData} = require("./getData");
+const {getData} = require("./getData");
 async function deleteProduct() {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
         terminal: false
     });
-
-
-    let dirPath = path.resolve(__dirname, "state");
-    const filePath = path.resolve(dirPath, "data.json");
     let answer = await get.getInput(rl);
-    product.splice(answer,1);
-    const newJsonContent = JSON.stringify(product,null, 2);
-    fs.mkdirSync(dirPath, {recursive: true});
-    fs.writeFileSync(filePath, newJsonContent);
-    // console.log(`продукт с id ${answer} был удален!`);
+    let products = getData();
+    products.splice(answer,1);
+    setData(products);
+
 }
 
 module.exports = {
