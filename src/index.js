8ede3697f492795d991957f9709c2780355053d2
rich-alien <Richalien@yandex.ru>
2020-12-04 01:40:39 +0400

    запуск express

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const requestLogger = require('./middlewares/request-logger.middleware');
+const controllers = require('./controllers');
+
+const PORT = 3000;
+
+const server = express()
+server.set('view engine', 'ejs');
+server.set('views', path.resolve(__dirname, 'views'));
+
+server.use(express.static(path.resolve(__dirname, 'style')));
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(requestLogger);
+
+server.use(controllers);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
