a8b968bb4d91e1caa590e2bcacd789cff3f6971b
rich-alien <Richalien@yandex.ru>
2020-12-03 19:19:59 +0400

    удален метод чтения , изменен подход добавления

--- addProduct.js
+++ addProduct.js
@@ -1,11 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+const {setData} = require("./getData");
 const readline = require("readline");
 const get = require("./getInput");
-let i = 0;
-let content;
+const {getData} = require("./getData");
+
 async function addProduct() {
-    i++;
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -19,41 +17,17 @@ async function addProduct() {
     let getPrice = await get.getInput(rl);
     console.log("Введите описание товара");
     let getDescription = await get.getInput(rl);
-
-    let dirPath = path.resolve(__dirname, "state");
-    const filePath = path.resolve(dirPath, "data.json");
-    const file = readFile(filePath);
-    content = file && JSON.parse(file) || [];
-    content.push({
+    let product = getData();
+    product.push({
         name: getName,
         count: getCount,
         price: getPrice,
         description: getDescription,
-    });
-
-    const jsonContent = JSON.stringify(content, null, 2);
-    fs.mkdirSync(dirPath, {recursive: true});
-    fs.writeFileSync(filePath, jsonContent);
+    })
+    setData(product);
 }
 
-
-
-function readFile(filePath) {
-    if(fs.existsSync(filePath)){
-        return fs.readFileSync(filePath);
-    }
-    return null;
-}
-const product = require("./state/data");
-function readProduct(){
-    console.log("--> Чтение товара")
-    if(i>0){
-        console.table(content);
-    }else console.table(product);
-
-}
 module.exports = {
-    addProduct,
-    readProduct
+    addProduct
 }
 
