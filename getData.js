3ee36806da6e589faf63a442268814f0c360737a
rich-alien <Richalien@yandex.ru>
2020-12-03 19:20:51 +0400

    добавлены 3  метода , первый для чтения , второй добавление и третий , при закрытии программы

---
+++ getData.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+let content = require('./state/data.json');
+
+ function getData(){
+    return content;
+}
+ function setData(product){
+    content = product;
+
+}
+ function setJSONData(){
+     let dirPath = path.resolve(__dirname, "state");
+     const filePath = path.resolve(dirPath, "data.json");
+     const jsonContent = JSON.stringify(content, null, 2);
+     fs.mkdirSync(dirPath, {recursive: true});
+     fs.writeFileSync(filePath, jsonContent);
+}
+
+module.exports={
+    getData,
+    setData,
+    setJSONData
+}
