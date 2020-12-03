a19986eef95c82ed75989254f1c963344a13cfc9
rich-alien <Richalien@yandex.ru>
2020-12-04 03:22:23 +0400

    создание роутера на просомтр продуктов

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const homeController = require('./home.controller');
-// const usersController = require('./users.controller');
-// const apiControllers = require('./api');
+const productController = require('./product.controller');
+
 
 const router = new Router();
 
 router.use('/', homeController);
-// router.use('/products', usersController);
-// router.use('/api', apiControllers);
+router.use('/view', productController);
+
 
 module.exports = router;
