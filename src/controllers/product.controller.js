8be0baa5361293729dd9072a9f899a35a7c35943
rich-alien <Richalien@yandex.ru>
2020-12-04 05:19:43 +0400

    ренедеринг всех старниц

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,13 +1,25 @@
+const {addProduct} = require("../repositories/addProduct");
 const { Router } = require('express');
 const {readProduct} = require("../repositories/readProduct")
 
 const router = new Router();
 
-router.get('/', async (_request, response) => {
-
+router.get('/view', async (_request, response) => {
     const data = readProduct();
     response.render('pages/product/view', { data });
 });
-
+router.get('/add', async (_request, response) => {
+    response.render('pages/product/add');
+});
+router.post('/add', async (request, response) => {
+    await addProduct(request.body);
+    response.redirect('/product');
+});
+router.get('/edit', async (_request, response) => {
+    response.render('pages/product/edit');
+});
+router.get('/delete', async (_request, response) => {
+    response.render('pages/product/delete');
+});
 
 module.exports = router;
