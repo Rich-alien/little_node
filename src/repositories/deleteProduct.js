d0e5d0f0a9eb3b0d2368ec5ea59509098c8cbbe8
rich-alien <Richalien@yandex.ru>
2020-12-04 10:32:42 +0400

    delete un use function

--- src/repositories/deleteProduct.js
+++ src/repositories/deleteProduct.js
@@ -1,10 +1,8 @@
-const get = require("./getInput");
 const {setData,getData} = require("./getData");
 async function deleteProduct() {
     let products = getData();
     products.splice(answer,1);
     setData(products);
-
 }
 
 module.exports = {
--- src/repositories/getInput.js
+++
@@ -1,9 +0,0 @@
-const getInput = function(rl) {
-    return new Promise(resolve => {
-        rl.question("> ", answer => {
-            resolve(answer);
-        })
-    })
-
-}
-module.exports.getInput = getInput;
