1774f1cbf86ddd891d0c2019057bf7cef4d42b70
rich-alien <Richalien@yandex.ru>
2020-12-04 06:09:07 +0400

    post а не get

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,3 +1,4 @@
+const {changeProduct} = require("../repositories/changeProduct");
 const {addProduct} = require("../repositories/addProduct");
 const { Router } = require('express');
 const {readProduct} = require("../repositories/readProduct")
@@ -8,16 +9,27 @@ router.get('/view', async (_request, response) => {
     const data = readProduct();
     response.render('pages/product/view', { data });
 });
+
+
 router.get('/add', async (_request, response) => {
     response.render('pages/product/add');
 });
-router.post('/add', async (request, response) => {
-    await addProduct(request.body);
+router.post('/add', async (_request, response) => {
+    await addProduct(_request.body);
     response.redirect('/product/view');
 });
+
+
 router.get('/edit', async (_request, response) => {
     response.render('pages/product/edit');
 });
+router.post('/edit', async (_request, response) => {
+    await changeProduct(_request.body);
+    response.redirect('/product/view');
+});
+
+
+
 router.get('/delete', async (_request, response) => {
     response.render('pages/product/delete');
 });
