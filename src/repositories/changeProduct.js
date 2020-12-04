3e5b3ae4aa81030a775e0603344471bdaa9b80dd
rich-alien <Richalien@yandex.ru>
2020-12-04 06:08:55 +0400

    изменена логика редактирования товара

--- src/repositories/changeProduct.js
+++ src/repositories/changeProduct.js
@@ -1,9 +1,16 @@
+const {getData} = require("./getData");
 const {addProduct} = require("./addProduct");
 const {deleteProduct} = require("./deleteProduct");
 
-async function changeProduct() {
-    await deleteProduct();
-    await addProduct();
+async function changeProduct(data) {
+    getData().splice(data.id,1);
+    let product = {
+        name : data.name,
+        count : data.count,
+        price : data.price,
+        description : data.description,
+    }
+    await addProduct(product);
 }
 
 module.exports = {
