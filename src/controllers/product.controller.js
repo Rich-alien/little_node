a84574ae7e3baa5fe5afade7caa45138ae3d7821
rich-alien <Richalien@yandex.ru>
2020-12-06 19:57:12 +0400

    not worker js, later uninstall

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,37 +1,63 @@
-const {changeProduct} = require("../repositories/changeProduct");
-const {addProduct} = require("../repositories/addProduct");
+
 const { Router } = require('express');
-const {readProduct} = require("../repositories/readProduct")
+const usersRepository = require('../repositories/usersRepository');
 
 const router = new Router();
 
-router.get('/view', async (_request, response) => {
-    const data = readProduct();
-    response.render('pages/product/view', { data });
+router.get('/', async (_request, response) => {
+    const users = await usersRepository.getAll();
+    response.render('pages/product/view', { users });
 });
 
-
-router.get('/add', async (_request, response) => {
+router.get('/add', (_request, response) => {
     response.render('pages/product/add');
 });
-router.post('/add', async (_request, response) => {
-    await addProduct(_request.body);
-    response.redirect('/product/view');
-});
 
-
-router.get('/edit', async (_request, response) => {
-    response.render('pages/product/edit');
-});
-router.post('/edit', async (_request, response) => {
-    await changeProduct(_request.body);
-    response.redirect('/product/view');
-});
-
-
-
-router.get('/delete', async (_request, response) => {
-    response.render('pages/product/delete');
+router.post('/add', async (request, response) => {
+    await usersRepository.add(request.body);
+    response.redirect('pages/product/view');
 });
 
 module.exports = router;
+// const {changeProduct} = require("../repositories/changeProduct");
+// const {addProduct} = require("../repositories/addProduct");
+// const { Router } = require('express');
+// const {readProduct} = require("../repositories/readProduct")
+//
+// const router = new Router();
+//
+// router.get('/view', async (_request, response) => {
+//     const data = readProduct();
+//     response.render('pages/product/view', { data });
+// });
+//
+//
+// // router.get('/add', async (_request, response) => {
+// //     response.render('pages/product/add');
+// // });
+// // router.post('/add', async (_request, response) => {
+// //     await addProduct(_request.body);
+// //     response.redirect('/product/view');
+// // });
+// router.post('/addProduct',async (_request, response) => {
+//     const products = addProduct(_request.body);
+//     response
+//         .status(201)
+//         .json(products);
+// });
+//
+// // router.get('/edit', async (_request, response) => {
+// //     response.render('pages/product/edit');
+// // });
+// // router.post('/edit', async (_request, response) => {
+// //     await changeProduct(_request.body);
+// //     response.redirect('/product/view');
+// // });
+// //
+// //
+// //
+// // router.get('/delete', async (_request, response) => {
+// //     response.render('pages/product/delete');
+// // });
+// //
+// // module.exports = router;
