80dccd217075600218907bfa85f1391658cb6a86
rich-alien <Richalien@yandex.ru>
2020-12-06 19:54:12 +0400

    rename puth in render file

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', (_request, response) => {
-    response.render('pages/home');
+    response.render('pages/viewProduct');
 });
 
 module.exports = router;
