3c90e9b841f4cdb1a78d07ecbba4134d80e6f5ec
rich-alien <Richalien@yandex.ru>
2020-12-06 22:56:46 +0400

    render page add

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,5 +5,11 @@ const router = new Router();
 router.get('/', (_request, response) => {
     response.render('pages/viewProduct');
 });
+router.get('/add', (_request, response) => {
+    response.render('pages/addProduct');
+});
+router.post('/add',(_request, response) => {
+    response.render('pages/viewProduct');
+});
 
 module.exports = router;
