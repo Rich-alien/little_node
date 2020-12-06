c09e835e79666dcb6110be2cf407d8aa29044e7d
rich-alien <Richalien@yandex.ru>
2020-12-06 19:56:31 +0400

    temporary addition from another repository of the function , to work ajax

--- src/controllers/api/product.controller.js
+++ src/controllers/api/product.controller.js
@@ -1,13 +1,28 @@
+const usersRepository = require("../../repositories/usersRepository");
 const { Router } = require('express');
-const {readProduct} = require("../../repositories/readProduct");
+// const {readProduct} = require("../../repositories/readProduct");
 
 const router = new Router();
 
 router.get('/', async (_request, response) => {
-    const data = readProduct();
-    response.json(data);
+    const products = await usersRepository.getAll();
+    response.json(products);
 });
+router.get('/:id', async(request, response) => {
+    const product = await usersRepository.get(request.params.id);
+    if (product) {
+        response.json(product);
+        return;
+    }
 
+    response.status(404);
+});
+router.post('/', async (request, response) => {
+    const user = await usersRepository.add(request.body);
+    response
+        .status(201)
+        .json(user);
+});
 // router.get('/:id', async(request, response) => {
 //     const user = await usersRepository.get(request.params.id);
 //     if (user) {
