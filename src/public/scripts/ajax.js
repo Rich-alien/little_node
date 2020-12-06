1eed06ff853a11780cd5c2ee83d6dd9d5605b5d5
rich-alien <Richalien@yandex.ru>
2020-12-06 19:52:32 +0400

    Recommended qyery selecyor for button, data output has been changed

--- src/public/scripts/ajax.js
+++ src/public/scripts/ajax.js
@@ -1,22 +1,25 @@
-const getProducts = () =>{
-    fetch("/product/view")
-        .then(response=>response.json())
-        .then(products=>{
+const getProducts = () => {
+    fetch("api/products")
+        .then(response => response.json())
+        .then(products => {
             let textBlock = document.querySelector(".response");
-            textBlock.innerHTML ="";
-            products.forEach(item=>{
-                textBlock.innerHTML +=`<span> product id: ${item.id} </span>
-<span> product id: ${item.name} </span>
-<span> product id: ${item.count} </span>
-<span> product id: ${item.price} </span>
-<span> product id: ${item.description} </span>
+            textBlock.innerHTML = "";
+            products.forEach((item,id) => {
+                textBlock.innerHTML += ` 
+            <tr>
+                <td>${ id} </td>
+                <td>${ item.name} </td>
+                <td>${ item.count} </td>
+                <td>${ item.price} </td>
+                <td>${ item.description} </td>
+            </tr>
+  
 `
-
             });
         });
 };
-const addProducts = () =>{
-    fetch("/product/view",{
+const addProducts = () => {
+    fetch("api/products", {
         method: "POST",
         body: JSON.stringify({
             name: "Some name",
@@ -24,17 +27,17 @@ const addProducts = () =>{
             price: "g",
             description: "f"
         }),
-        headers:{
+        headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-        .then((response)=>response.json())
-        .then((json)=> {
+        .then((response) => response.json())
+        .then((json) => {
             getProducts();
             console.log(json);
         })
 }
-let buttonGetProducts = document.querySelector(".button-get-products");
-let buttonAddProducts = document.querySelector(".button-add-products");
-buttonGetProducts.addEventListener("click",getProducts);
-buttonAddProducts.addEventListener("click",addProducts);
+// let buttonGetProducts = document.querySelector(".button-get-products");
+// let buttonAddProducts = document.querySelector(".button-add-products");
+// buttonGetProducts.addEventListener("click", getProducts);
+// buttonAddProducts.addEventListener("click", addProducts);
