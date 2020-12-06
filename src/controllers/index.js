e76933b725daef3fee2ff2537ef9dd69a3b863a0
rich-alien <Richalien@yandex.ru>
2020-12-06 19:54:52 +0400

    add /api for ajax job

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const homeController = require('./home.controller');
 const productController = require('./product.controller');
+const apiController = require('./api');
 
 
 const router = new Router();
 
 router.use('/', homeController);
 router.use('/product', productController);
+router.use('/api', apiController);
 
 
 module.exports = router;
