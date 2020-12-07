154167455d24993bee68e8733acff72ace1b6e82
rich-alien <Richalien@yandex.ru>
2020-12-07 05:01:03 +0400

    изменены пути

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,16 +5,19 @@ const router = new Router();
 router.get('/', (_request, response) => {
     response.render('pages/viewProduct');
 });
+router.delete('/', (_request, response) => {
+    response.render('pages/viewProduct');
+});
 router.get('/add', (_request, response) => {
     response.render('pages/addProduct');
 });
 router.post('/add',(_request, response) => {
-    response.render('pages/viewProduct');
+    response.render('pages/addProduct');
 });
 router.get('/edit', (_request, response) => {
     response.render('pages/editProduct');
 });
 router.post('/edit',(_request, response) => {
-    response.render('pages/viewProduct');
+    response.render('pages/editProduct');
 });
 module.exports = router;
