95090e1509aaae649bda2bf18c18a8da8d579aa2
rich-alien <Richalien@yandex.ru>
2020-12-07 05:00:27 +0400

    добавлена функции по изменению данных

--- src/public/scripts/ajax.js
+++ src/public/scripts/ajax.js
@@ -41,6 +41,31 @@ const addProducts = () => {
             console.log(json);
         })
 }
+const editProducts = () => {
+    let id = document.getElementById("name").id;
+    let name = document.getElementById("name").value;
+    let count = document.getElementById("count").value;
+    let price = document.getElementById("price").value;
+    let description = document.getElementById("description").value;
+    fetch("api/products", {
+        method: "POST",
+        body: JSON.stringify({
+            id: id,
+            name: name,
+            count: count,
+            price: price,
+            description: description
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            getProducts();
+            console.log(json);
+        })
+}
 const deleteProduct = (id) => {
     let product = document.querySelector('.response');
     console.log(product);
