90218de3ffa69a3a524c1bf02f89e8f38216db54
rich-alien <Richalien@yandex.ru>
2020-12-17 14:18:56 +0400

    using new script and add new packages

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        bundle: path.resolve(__dirname, "src/scripts/main.ts")
+    },
+    devtool: 'inline-source-map',
+    output: {
+        publicPath: "/",
+        path: path.resolve(__dirname, "src/public"),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "less-loader",
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".less"]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "base.css"
+        })
+    ],
+
+    watchOptions: {
+        ignored: 'node_modules/**'
+    },
+};
