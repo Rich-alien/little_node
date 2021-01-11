f1a9f1bb2455b4b89254e68542ef9afa44e5850e
Richalien <Richalien@yandex.ru>
2021-01-12 02:27:50 +0400

    refactoring to ts

--- src/scripts/productLine.ts
+++ src/scripts/productLine.ts
@@ -1,30 +1,28 @@
-class Product {
-    _product;
-    wrapper;
-    _index;
-    _count;
-    newCounter;
+import { CountProduct} from "./count";
+import {Product} from "./product";
 
-    constructor(product, wrapper, index) {
-        this._product = product;
-        this.wrapper = wrapper;
-        this._index = index;
-        this.createProduct();
+export class ProductLine {
+    private _count;
+    private newCounter;
+
+    constructor(product: Product, wrapper: Element, index: number) {
+        this.createProduct(product,wrapper,index);
     }
 
-    createProduct() {
+    createProduct(product,wrapper,index) {
+        debugger;
         let template = document.createElement("div");
         template.className = ("basketPopup");
-        template.innerHTML = `<p class="basket__name">${this._product.name}</p>
-                              <p class="basket__country">${this._product.country}</p>
-                              <p class="basket__price">${this._product.price * this._product.count}</p>
+        template.innerHTML = `<p class="basket__name">${product.name}</p>
+                              <p class="basket__country">${product.country}</p>
+                              <p class="basket__price">${product.price * product.count}</p>
            `;
-        this.newCounter = new Count(this._product.count, template);
-        this.wrapper.append(template);
-        this.newCounter.updatePrice = (count) => {
+        const counter : CountProduct = new CountProduct(product.count, template);
+        wrapper.append(template);
+        counter.updatePrice = (count : number) => {
             this._count = count;
-            this.updateCount(this._count);
-            template.querySelector(".basket__price").innerHTML = this._count * this._product.price;
+            counter.updatePrice(this._count);
+            template.querySelector(".basket__price").innerHTML = String(this._count * product.price);
         }
     }
 }
