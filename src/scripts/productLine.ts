f70864fdf78d2adf0361427c186cf8bbfd0c852f
Richalien <Richalien@yandex.ru>
2021-01-12 04:54:23 +0400

    удален вывод в html о стране в корзине , а так же восстановлена функция обновления ценника

--- src/scripts/productLine.ts
+++ src/scripts/productLine.ts
@@ -6,23 +6,23 @@ export class ProductLine {
     private newCounter;
 
     constructor(product: Product, wrapper: Element, index: number) {
-        this.createProduct(product,wrapper,index);
+        this.createProduct(product,wrapper);
     }
 
-    createProduct(product,wrapper,index) {
-        debugger;
+    createProduct(product,wrapper) {
         let template = document.createElement("div");
+
         template.className = ("basketPopup");
         template.innerHTML = `<p class="basket__name">${product.name}</p>
-                              <p class="basket__country">${product.country}</p>
-                              <p class="basket__price">${product.price * product.count}</p>
-           `;
+                              <p class="basket__price">${product.price * product.count}`;
+
         const counter : CountProduct = new CountProduct(product.count, template);
         wrapper.append(template);
-        counter.updatePrice = (count : number) => {
-            this._count = count;
-            counter.updatePrice(this._count);
-            template.querySelector(".basket__price").innerHTML = String(this._count * product.price);
-        }
     }
+
 }
+// counter.updatePrice = (count : number) => {
+//     this._count = count;
+//     counter.updatePrice(this._count);
+//     template.querySelector(".basket__price").innerHTML = String(this._count * product.price);
+// }
