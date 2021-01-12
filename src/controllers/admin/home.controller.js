c982dc4185e7f4dd8536a445e3a9959336b363bb
Richalien <Richalien@yandex.ru>
2021-01-12 07:48:28 +0400

    изменен путь для рендера

--- src/controllers/admin/home.controller.js
+++ src/controllers/admin/home.controller.js
@@ -6,13 +6,13 @@ router.get('/', (_request, response) => {
     response.render('adminPanel/pages/home');
 });
 router.delete('/', (_request, response) => {
-    response.render('pages/viewProduct');
+    response.render('adminPanel/pages/viewProduct');
 });
 router.get('/add', (_request, response) => {
-    response.render('pages/addProduct');
+    response.render('adminPanel/pages/addProduct');
 });
 router.post('/add',(_request, response) => {
-    response.render('pages/addProduct');
+    response.render('adminPanel/pages/addProduct');
 });
 router.get('/edit',(_request, response) => {
     response.render('adminPanel/pages/editProduct');
@@ -21,9 +21,9 @@ router.post('/edit', (_request, response) => {
     response.render('adminPanel/pages/editProduct');
 });
 router.get('/partialEdit',(_request, response) => {
-    response.render('pages/partialEdit');
+    response.render('adminPanel/pages/partialEdit');
 });
 router.post('/partialEdit', (_request, response) => {
-    response.render('pages/partialEdit');
+    response.render('adminPanel/pages/partialEdit');
 });
 module.exports = router;
