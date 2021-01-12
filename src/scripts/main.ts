4fa0aeafa5cbcc743f8f862875d0fb12a2ad6b4b
Richalien <Richalien@yandex.ru>
2021-01-12 06:31:19 +0400

    fix

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,29 +4,34 @@ import {Popup} from "./popUp";
 import '../assets/themes/base/styles/base.less'
 // let click = 0;
 let cart = new Map;
-let basketCount = $(".basket__counter");
 let preBasket = [];
-let newPopup = new Popup(preBasket);
+
+let newPopup= new Popup(preBasket);
 
 
 
 document.onclick = (event: MouseEvent) => {
+    // console.log(<HTMLElement>event.target);
     if ((<HTMLElement>event.target).classList.contains('product-card__button')) {
-        // $('.basketPopup').remove();
         const button: HTMLElement = <HTMLElement>event.target;
         let id: number = +button.attributes[0].value;
         importToCart(id);
     }
+    // else if((<HTMLElement>event.target).classList.contains('header_basket')) {
+    //     $('.basketPopup').remove();
+    //     newPopup= new Popup(preBasket);
+    // }
     else{
         console.log('');
     }
 
 };
 //проверка на совпадение эллементов
-
+let click = 0;
 let importToCart = (id: number) => {
     if (!cart.has(id)) {
         cart.set(id, data[id]);
+        $('.basketPopup').remove();
         newPopup.mapProductData(cart);
     } else {
         cart.get(id).count++;
